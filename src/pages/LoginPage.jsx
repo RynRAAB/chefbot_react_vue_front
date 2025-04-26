9d0daea1aa505b3cc6a027d880e7d7e0dfb30ddd
Rayane RAAB <raabrayane44@gmail.com>
2025-04-26 02:37:31 +0200

    login update

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -25,14 +25,14 @@ export default function LoginPage() {
             },
           });
           if (response.data.message === "Token valide")
-            window.location.href= "https://chefbot-tfm1.onrender.com/chatpage";
+            window.location.href= "https://chefbot-front.onrender.com/chatpage";
           else {
             localStorage.removeItem("token");
-            window.location.href= "https://chefbot-tfm1.onrender.com/login";
+            window.location.href= "https://chefbot-front.onrender.com/login";
           }
         } catch(error) {
           localStorage.removeItem("token");
-          window.location.href= "https://chefbot-tfm1.onrender.com/login";
+          window.location.href= "https://chefbot-front.onrender.com/login";
         }
       }
     };
